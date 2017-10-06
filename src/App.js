2b19c16d96297df7feee98e275c354308e629211
Nemolite <g25092011@mail.ru>
2017-10-06 14:02:35 +0300

    component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-	  Working!
+      <div style={{padding: 100}}>
+	  <RaisedButton label="Кнопка" />
 	  </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+injectTapEventPlugin();
+
+ReactDOM.render( 
+
+ <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+
+
+,document.getElementById('root')
 );
